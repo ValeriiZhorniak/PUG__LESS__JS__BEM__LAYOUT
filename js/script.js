01a2c7cff192a10dbd36df2b30d6f563851bed15
Valerii Zhorniak <zhorniakvalerii@gmail.com>
2021-04-08 23:31:25 +0300

    menu

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-var elem = document.getElementById('id');
-function func (){
-	alert('ddddd')
+function check (){
+	var meat = document.getElementById('meat');
+	var fish = document.getElementById('fish');
+	var chicken = document.getElementById('chicken');
+	var check = document.getElementById('check');
+	if(check.checked){
+		meat.style.display = 'none';
+		fish.style.display = 'none';
+		chicken.style.display = 'none';
+	} else {
+		meat.style.display = 'flex';
+		fish.style.display = 'flex';
+		chicken.style.display = 'flex';
+	}
 }
